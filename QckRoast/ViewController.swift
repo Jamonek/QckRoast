279c285f292cc9a79dcf8f2117aae0737217a07a
Jamone Kelly <xg.monie089@gmail.com>
2014-10-12 14:58:51 -0400

    Initial Commit

---
+++ QckRoast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QckRoast
+//
+//  Created by Jamone Alexander Kelly on 10/12/14.
+//  Copyright (c) 2014 Jamone Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
